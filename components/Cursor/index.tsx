494d5f59fe14ffb99f0c66d5b49326a0a90090f3
Himanshu0422 <himanshumittal035@gmail.com>
2024-10-20 18:24:49 +0530

    removed browser cursor and added custom

---
+++ components/Cursor/index.tsx
@@ -0,0 +1,82 @@
+import { MutableRefObject, useEffect, useRef } from "react";
+import { gsap, Linear } from "gsap";
+import { IDesktop, isSmallScreen } from "@/app/page";
+import styles from "./styles.module.css";
+
+const CURSOR_STYLES = {
+  CURSOR: "fixed hidden bg-white w-4 h-4 select-none pointer-events-none z-50",
+  FOLLOWER: "fixed hidden h-8 w-8 select-none pointer-events-none z-50",
+};
+
+const Cursor = ({ isDesktop }: IDesktop) => {
+  const cursor: MutableRefObject<HTMLDivElement | null> = useRef(null);
+  const follower: MutableRefObject<HTMLDivElement | null> = useRef(null);
+
+  const onHover = () => {
+    gsap.to(cursor.current, {
+      scale: 0.5,
+      duration: 0.3,
+    });
+    gsap.to(follower.current, {
+      scale: 3,
+      duration: 0.5,
+    });
+  };
+
+  const onUnhover = () => {
+    gsap.to(cursor.current, {
+      scale: 1,
+      duration: 0.3,
+    });
+    gsap.to(follower.current, {
+      scale: 1,
+      duration: 0.3,
+    });
+  };
+
+  const moveCircle = (e: MouseEvent) => {
+    gsap.to(cursor.current, {
+      x: e.clientX,
+      y: e.clientY,
+      duration: 0.1,
+      ease: Linear.easeNone,
+    });
+    gsap.to(follower.current, {
+      x: e.clientX,
+      y: e.clientY,
+      duration: 0.3,
+      ease: Linear.easeNone,
+    });
+  };
+
+  const initCursorAnimation = () => {
+    follower.current?.classList.remove("hidden");
+    cursor.current?.classList.remove("hidden");
+
+    document.addEventListener("mousemove", moveCircle);
+
+    document.querySelectorAll(".link").forEach((el) => {
+      el.addEventListener("mouseenter", onHover);
+      el.addEventListener("mouseleave", onUnhover);
+    });
+  };
+
+  useEffect(() => {
+    if (isDesktop && !isSmallScreen()) {
+      initCursorAnimation();
+    }
+    return () => {
+      // Cleanup to avoid memory leaks
+      document.removeEventListener("mousemove", moveCircle);
+    };
+  }, [cursor, follower, isDesktop]);
+
+  return (
+    <>
+      <div ref={cursor} className={`${styles.cursor} ${CURSOR_STYLES.CURSOR}`}></div>
+      <div ref={follower} className={`${styles.cursorFollower} ${CURSOR_STYLES.FOLLOWER}`}></div>
+    </>
+  );
+};
+
+export default Cursor;
